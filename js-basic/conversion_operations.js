14245d22faa20798d466aac5485aee2e162f0ff5
Vandana-Bheda <167683582+Vandana-Bheda@users.noreply.github.com>
2024-09-04 07:09:44 +0000

    edited conversion and operations

--- js-basic/conversion_operations.js
+++ js-basic/conversion_operations.js
@@ -22,5 +22,47 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 let exNumber = 77;
 
 let stringNumber = String(exNumber);
-console.log(typeof stringNumber); // => string
-console.log(stringNumber); // => 77
+// console.log(typeof stringNumber); // => string
+// console.log(stringNumber); // => 77
+
+// *************operations*****************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello";
+let str2 = " vandana";
+
+let str = str1 + str2;
+// console.log(str); // => Hello vandana
+
+// console.log("1" + 2); // => 12
+// console.log(1 + "2"); // => 12
+// console.log("1" + 2 + 2); // => 122
+// console.log(2 + 2 + "1"); // => 41
+
+// console.log(true); // => true
+// console.log(+true); // => 1
+// console.log(+""); // => 0
+// console.log(true+); // => error
+
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+++gameCounter; // prefix
+gameCounter++; // postfix
+console.log(gameCounter);
+
+
+
