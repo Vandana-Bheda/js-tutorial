079396aa30cbe719c36f54333b9c3d2588c53dec
Vandana-Bheda <167683582+Vandana-Bheda@users.noreply.github.com>
2024-09-04 07:20:22 +0000

    comparisions

---
+++ js-basic/comparision.js
@@ -0,0 +1,18 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 === 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1); // => true
+// console.log("02" > 1); // => true
+
+// console.log(null > 0); // => false  //avoid 
+// console.log(null == 0); // => false //avoid 
+// console.log(null >= 0); // => true //avoid 
+
+// === strict comparision means it compares data types also
+
+// console.log("2" == 2); // => true
+console.log("2" === 2); // => false
