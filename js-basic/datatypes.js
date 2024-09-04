ae6a0d8e3bc4715054aa9dac811f2def4b9fc224
Vandana-Bheda <167683582+Vandana-Bheda@users.noreply.github.com>
2024-09-04 06:21:36 +0000

    datatypes

---
+++ js-basic/datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all js code as newer version
+
+// alert( 3 + 3) // we are using nodejs , not browser
+
+console.log(3
+    +3); // code readability should be high
+
+console.log("vandana");
+
+let name = "vandana";
+let age =  20;
+let isLoggedIn = false;
+
+// number
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value 
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof "vandana");
+console.log(typeof null);
+console.log(typeof undefined);
+
