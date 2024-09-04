e3f00aa8c2bd44e0ed9060c487ebc8a14d257a70
Vandana-Bheda <167683582+Vandana-Bheda@users.noreply.github.com>
2024-09-04 06:04:13 +0000

    let const var

--- js-basic/test.js
+++ js-basic/test.js
@@ -1,1 +1,16 @@
 console.log("vandana");
+
+let studentName = "Vandana";
+var studentAge = 20;
+const studentStream = "It Web";
+studentHobby = "Coading";
+let studenCtc;
+// prefer not to use var
+// because of issue in block scope and functional scope
+
+console.log(studentName);
+console.log(studentAge);
+console.log(studentStream);
+console.log(studentHobby);
+
+console.table([studentName,studentAge,studentStream,studentHobby,studenCtc]);
