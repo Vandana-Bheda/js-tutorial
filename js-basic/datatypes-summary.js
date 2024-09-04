30d307ec9cd307a8e65b1c7f4c51258887bd0a4f
Vandana-Bheda <167683582+Vandana-Bheda@users.noreply.github.com>
2024-09-04 07:39:35 +0000

    datatypes summary

---
+++ js-basic/datatypes-summary.js
@@ -0,0 +1,31 @@
+// primitive
+
+//7 types : string, number, boolean, null, undefined, symbol, BigInt
+
+const name = "vandana"; // string
+const score = 100; // number
+const isLoggedIn = true; // boolean
+const years = null; // null
+let age; //undefined
+
+const id = Symbol('123'); //symbol
+const newId = Symbol('123'); //symbol
+
+// console.log(id === newId); //false
+
+const bigNumber = 235345346346544n; //bigint
+
+//reference (non premetive)
+
+// arrays, objects, functions
+
+const heros = ["shaktiman", "naagraj", "doga"]; // array
+
+let newObj = {
+    name: "vandana",
+    age: 22,
+} // object
+
+const myFunction = function(){
+    console.log("hello world");
+}
