b5f87dbb85b3397fdda9559d626a4c7fb717acca
Vandana-Bheda <167683582+Vandana-Bheda@users.noreply.github.com>
2024-09-04 06:42:54 +0000

    conversion and operations

---
+++ js-basic/conversion_operations.js
@@ -0,0 +1,26 @@
+let score = "33abc"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let ValueInNumber = Number(score);
+//console.log(ValueInNumber);
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1 ; false => 0
+
+// let isLoggedIn = 1 // true 
+// let isLoggedIn = 0 // false 
+// let isLoggedIn = "" // false 
+let isLoggedIn = "vandana" // true 
+
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+let exNumber = 77;
+
+let stringNumber = String(exNumber);
+console.log(typeof stringNumber); // => string
+console.log(stringNumber); // => 77
