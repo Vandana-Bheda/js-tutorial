b9e277efd0ec05921033d79862f581691af27210
Vandana-Bheda <167683582+Vandana-Bheda@users.noreply.github.com>
2024-09-19 13:18:55 +0000

    do and do while loops

---
+++ 05_iterations/do_dowhile.js
@@ -0,0 +1,21 @@
+let i = 1;
+while (i <= 10) {
+    // console.log(`value of i is ${i}`);
+    i = i + 2;
+}//1 3 5 7 9
+
+let heros = ["batman","superman","flash"];
+let arr = 0;
+while (arr<heros.length) {
+    // console.log(`value is ${heros[arr]}`);
+    arr++;
+}//value is batman value is supermna value is flash
+
+
+//do while loop
+
+let score = 1;
+do {
+    console.log(`score is ${score}`);
+    score++;
+} while (score<=10);// score is 1 to 10
